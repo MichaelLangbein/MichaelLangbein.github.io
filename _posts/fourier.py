7bb7dc8cd2980205cdb0928d0ba3321013ea7fa9
rinso86 <michael_langbein@hotmail.de>
2018-03-11 22:04:39 +0100

    fourier stuff

--- _posts/fourier.py
+++ _posts/fourier.py
@@ -1,9 +1,79 @@
-import numpy as np
-
-def ellipse(theta, phi, r1, r2, r3):
-    x = r1 * np.cos(theta) * np.cos(phi)
-    y = r2 * np.cos(theta) * np.sin(phi)
-    z = r3 * np.sin(theta)
-    return x, y, z
+from numpy import zeros, exp, pi, complex128, shape, fft, abs
+import matplotlib.pyplot as plt
 
 
+def amplitudeFunc(fr1, fr2, signal):
+    X, Y = shape(signal)
+    sm = 0
+    for x in range(X):
+        for y in range(Y):
+            sm += signal[x][y] * exp( -1j * 2.0 * pi * ( x*fr1/X + y*fr2/Y ) )
+    return sm
+
+
+def fourierTransform(signal):
+    N, M = shape(signal)
+    amps = zeros((N, M), dtype=complex128)
+    for n in range(N):
+        for m in range(M):
+            amps[n][m] = amplitudeFunc(n, m, signal)
+    return amps
+
+
+def fourierTransformInverse(amps):
+    N, M = shape(amps)
+    signal = zeros((N, M), dtype=complex128)
+    for n in range(N):
+        for m in range(M):
+            signal[n][m] = 1.0 / amplitudeFunc(n, m, amps)
+    return signal
+
+
+def getMax(mtrx):
+    mx = 0
+    N, M = shape(mtrx)
+    for n in range(N):
+        for m in range(M):
+            if abs(mtrx[n][m]) > mx:
+                mx = mtrx[n][m]
+    return mx
+
+
+def filterBy(dataIn, filterFunc):
+    N, M = shape(signal)
+    dataOut = zeros((N, M), dtype=complex128)
+    for n in range(N):
+        for m in range(M):
+            if filterFunc(dataIn[n][m]):
+                dataOut[n][m] = dataIn[n][m]
+    return dataOut
+
+
+def plotFour(s1, a1, s2, a2):
+    plt.figure(1)
+    plt.subplot(221)
+    plt.imshow(abs(s1))
+    plt.subplot(222)
+    plt.imshow(abs(a1))
+    plt.subplot(223)
+    plt.imshow(abs(s2))
+    plt.subplot(224)
+    plt.imshow(abs(a2))
+    plt.show()
+
+
+# Step 1: create data
+signal = [[1,2],[3,4]]
+
+# Step 2: transform
+amps = fourierTransform(signal)
+
+# Step 3: manipulate
+thrsh = getMax(amps) * 0.95
+ampsNew = filterBy(amps, lambda val : val > thrsh)
+
+# Step 4: backtransform
+signalNew = fourierTransformInverse(ampsNew)
+
+# Step 5: plot
+plotFour(signal, amps, signalNew, ampsNew)
